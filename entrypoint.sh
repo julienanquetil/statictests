a606a8c252f7472e431e988ccfc90b87491952c4
Julien ANQUETIL <julien.anquetil@gmail.com>
2021-10-20 21:08:40 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,5 @@ cp /problem-matcher.json ${HOME}/
 echo "::add-matcher::${HOME}/problem-matcher.json"
 
 cd $GITHUB_WORKSPACE
-sh -c "/root/.composer/vendor/bin/phpcs --report=checkstyle --standard=Magento2 $GITHUB_WORKSPACE/$1 -s $*"
+test -z "${INPUT_PHPCS_STANDARD}" && INPUT_PHPCS_STANDARD=Magento2
+sh -c "/root/.composer/vendor/bin/phpcs --report=checkstyle --standard=$INPUT_PHPCS_STANDARD $GITHUB_WORKSPACE/$1 -s $*"
